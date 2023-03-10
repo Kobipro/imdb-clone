265bc9a82589989d3a9357ef7fda71259da82a88
kobipro <Boatengkwabenaakuamoah@gmail.com>
2023-03-10 20:31:02 +0000

    version 1 commit

--- next.config.js
+++ next.config.js
@@ -1,8 +1,19 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   experimental: {
+//     appDir: true,
+//   },
+// }
+
+// module.exports = nextConfig
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  images: {
+    domains: ["image.tmdb.org"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
